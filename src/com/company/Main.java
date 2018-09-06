c860f4bdbf41ea82cbd62bfa0c89e6cc114578e6
Jordyn <jordyn.robinson.2021@student.lhschools.org>
2018-09-06 09:05:23 -0400

    Initial commit, madlib file is in the src folder

---
+++ src/com/company/Main.java
@@ -0,0 +1,99 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main
+
+    {public static void main(String[] args) {
+         //declare
+        String greeting;
+        String name;
+        String noun
+        String adjective
+        //initializing
+        greeting = "Hi";
+        /*BTS is my life. they have showed me how to love myself and find happiness in life
+        ..
+          a dash of (noun)
+          A cup of (adjective)
+          Kookie for (verb)
+          The full (noun)
+          a (noun) so (adjective)
+          Jin and Chim so sweet
+          We Jhope your day
+          is fantastic and slay all (verb)
+          Rm, the god of (animal) who seems to break everything he (verb)
+          Jin, a pink princess who loves his dad (noun)
+          Suga, an (animal) who seems (noun) but has his own (noun)
+          JHope. a ray of (noun)that gives off sunshine
+          Jimin, a small little (adjective) who can be cute and (verb) at the same time
+          V, a cutie (animal) who makes other people (noun) alot
+          Jungkook, the golden (animal) who has always been talented from the start
+          BTS, the amazing (noun)that will be our boys (verb)
+
+
+
+
+
+
+
+
+
+
+          */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Scanner keyboard;
+         String greetings;
+
+
+
+
+
+
+        keyboard= new Scanner(System.in);
+
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           }
+
+}
