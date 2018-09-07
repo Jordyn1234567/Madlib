4f08435d25faa2978a7704d6267fe6a3ef67a886
Jordyn1234567 <42771680+Jordyn1234567@users.noreply.github.com>
2018-09-07 02:14:55 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,10 +12,29 @@ public class Main
         String adjective
         //initializing
         greeting = "Hi";
+        person = "userperson";
+        
+        
+        
+        
+        System.out.pringln("Hello! Welcome to my MadLib.");
+        username= keyboard.nextline();
+        System.out.printlh(greeting + " " +username);
+        System.out.println ("Give me a noun");
+        userPerson= keyboard.nextline()
+        System.out.println(person + " "+username);
+        
+        
+        
+        
+        
+        
+        
+        
         /*BTS is my life. they have showed me how to love myself and find happiness in life
         ..
           a dash of (noun)
-          A cup of (adjective)
+          A cup of (noun)
           Kookie for (verb)
           The full (noun)
           a (noun) so (adjective)
@@ -24,12 +43,31 @@ public class Main
           is fantastic and slay all (verb)
           Rm, the god of (animal) who seems to break everything he (verb)
           Jin, a pink princess who loves his dad (noun)
-          Suga, an (animal) who seems (noun) but has his own (noun)
-          JHope. a ray of (noun)that gives off sunshine
-          Jimin, a small little (adjective) who can be cute and (verb) at the same time
+          Suga, an (animal) who seems (adjective) but has his own (noun)
+          JHope. a ray of sunshien that gives off (noun)
+          Jimin, a (adjective) little pepper who can be cute and can (verb) alot
           V, a cutie (animal) who makes other people (noun) alot
           Jungkook, the golden (animal) who has always been talented from the start
-          BTS, the amazing (noun)that will be our boys (verb)
+          BTS, the amazing (noun) that will be our boys (adverb)
+          
+          a dash of sugar
+          a cup of booty flakes
+          kookie for eating
+          the full big booty judy
+          a snail so slow
+          jin and chim so sweet
+          we jhope your day 
+          is fantastic and slay all jumpging in happiness
+          RM, thr god of tigers who seems dumb but has his own pet worm
+          Jin, a pink princess who loves his dad jokes
+          Suga, an lion who seems sad but has his own flow
+          JHope, a ray of sunshine that gives off hope
+          Jimin, a small little pepper who can be cute and can sleep alot 
+          V, a cutie koala who makes other people smile alot
+          Jungkook, the golden warrior whohas always been talented from the start
+          BTS, the amazing lion pride that will be our boys forever 
+     
+          
 
 
 
